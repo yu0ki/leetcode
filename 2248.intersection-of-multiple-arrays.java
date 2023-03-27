0baeced59de679fb2140387bb2d3fac45f99cfd8
yu0ki <49509576+yu0ki@users.noreply.github.com>
2023-03-27 09:48:06 +0900

    [clear]485

---
+++ 2248.intersection-of-multiple-arrays.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=2248 lang=java
+ *
+ * [2248] Intersection of Multiple Arrays
+ */
+
+// @lc code=start
+class Solution {
+    public List<Integer> intersection(int[][] nums) {
+        
+    }
+}
+// @lc code=end
+
