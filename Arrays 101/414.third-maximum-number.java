b68038ac2c57cc6cff7f3e2414505a8674bda084
yu0ki <49509576+yu0ki@users.noreply.github.com>
2023-03-28 17:01:15 +0900

    [accepted]414

--- Arrays 101/414.third-maximum-number.java
+++ Arrays 101/414.third-maximum-number.java
@@ -7,6 +7,51 @@
 // @lc code=start
 class Solution {
     public int thirdMax(int[] nums) {
+
+        int num_len = nums.length;
+
+        if (num_len == 1) { return nums[0]; } 
+        else if (num_len == 2) {
+            if (nums[0] > nums[1]) {
+                return nums[0];
+            } else {
+                return nums[1];
+            }
+        }
+
+        // M1みたいに一個ずつ検査していけばいいんじゃないか？
+        int first = nums[0];
+        int second = nums[0];
+        int third = nums[0];
+        int thirdIsReturned = 0;
+
+        for (int i = 1; i < num_len; i++) {
+            if (first < nums[i]) {
+                third = second;
+                second = first;
+                first = nums[i];
+                thirdIsReturned++;
+            } else if (second < nums[i] && nums[i] < first) {
+                third = second;
+                second = nums[i];
+                thirdIsReturned++;
+            } else if (third < nums[i] && nums[i] < second) {
+                third = nums[i];
+                thirdIsReturned++;
+            } else if (thirdIsReturned == 0 && nums[i] < first) {
+                second = nums[i];
+                thirdIsReturned++;
+            } else if (thirdIsReturned == 1 && nums[i] < second) {
+                third = nums[i];
+                thirdIsReturned++;
+            }
+        }
+
+        if (thirdIsReturned >= 2) {
+            return third;
+        } else {
+            return first;
+        }
         
     }
 }
